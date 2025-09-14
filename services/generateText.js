5b8b49069f6b92e4d68267122b03f09d0f4c488c
itgeljesus318-afk <itgeljesus318@gmail.com>
2025-09-15 00:26:30 +0800

    Update generateText.js

--- services/generateText.js
+++ services/generateText.js
@@ -1,6 +1,5 @@
-// services/generateText.js
 const generateText = async (childName, storyChoice) => {
-  // Return a simple placeholder story
+  // Simple placeholder story
   return `Once upon a time, ${childName} went on an adventure in the magical world of ${storyChoice}.`;
 };
 
