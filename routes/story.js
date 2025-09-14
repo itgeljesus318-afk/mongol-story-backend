f398df04b355e56ebf9b43d79854a28921d84ed3
itgeljesus318-afk <itgeljesus318@gmail.com>
2025-09-15 00:26:03 +0800

    Update story.js

--- routes/story.js
+++ routes/story.js
@@ -23,7 +23,7 @@ router.post("/create", async (req, res) => {
 
     return res.json({
       message: "Story created successfully",
-      pdfUrl: `https://<your-backend-url>/temp/story-${childName}.pdf`
+      pdfUrl: `https://https://mongol-story-backend.onrender.com/temp/story-${childName}.pdf`
     });
 
   } catch (err) {
